474df00d59098d105314b90ece93a88ad1002650
porto <portothree@gmail.com>
2019-10-12 11:16:10 +0100

    :tada: initial commit

---
+++ app/config.js
@@ -0,0 +1,54 @@
+
+'use strict'
+
+
+// Export
+
+module.exports = {
+
+	app: {
+		name: 'Gustavo Porto',
+	},
+
+	use: {
+		templates: '.html',
+		scripts: '.js',
+		styles: '.scss',
+	},
+
+	build: {
+		imagemin: [ 'png', 'jpg' ],
+		sourcemaps: [ 'js', 'css' ],
+		autoprefixer: [ 'last 3 versions', 'ie 10', 'ie 11' ],
+		mainLevel: "common",
+		mainBundle: "app",
+		globalStyles: "app/blocks/global.scss"
+	},
+
+	dist: {
+		styles: 'styles',
+		fonts: 'styles/fonts',
+		img: 'styles/img',
+		symbol: 'styles/img',
+		scripts: 'scripts',
+		static: 'static',
+		favicons: 'favicons',
+	},
+
+	favicons: {
+		android: false,
+		appleIcon: false,
+		appleStartup: false,
+		coast: false,
+		favicons: true,
+		firefox: false,
+		windows: false,
+		yandex: false,
+	},
+
+	HTMLBeautify: {
+		preserve_newlines: false,
+	},
+
+}
+
